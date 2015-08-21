19d562b0d27adb7039423654cd63694b4c0ffbb4
mlaugh4 <mwlaugh@gmail.com>
2015-08-21 08:43:04 +0300

    jquery

--- jquery.js
+++ jquery.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  $('.header').show().slideDown(500)
-})
+  $('.hero-image').slideDown(1000);
+  setTimeout($('.header').css("opacity","1"),1000);
+});
+
+$('.link').click(function() {
+  $('.header').css("padding-bottom","80vh");
+  $(this).addClass("active");
+});
