e30bb8b438be58c4c38211522b906d5bdbfbee51
mlaugh4 <mwlaugh@gmail.com>
2015-09-02 11:45:36 -0700

    sliding works now

--- jquery.js
+++ jquery.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
 
   $('.links2 > a:nth-child(1)').click(function(){
-    $('#mark').animate({width:'toggle',opacity:'toggle'},500,"swing");
+    // $('.container div:nth-child(2)').delay(500).hide(500);
+    $('#mark').animate({width:'toggle',opacity:'toggle'},500,"swing").delay(200);
     $(this).toggleClass("active1")
   });
 
   $('.links2 > a:nth-child(2)').click(function(){
-    if ($())
-    $('#mark').hide()
-    $('#work').animate({height:'toggle',opacity:'toggle'},500,"swing");
-    $(this).toggleClass("active2")
+    if ( $('#mark').is(":visible") ) {
+      $('#mark').animate({width:'toggle',opacity:'toggle'},{duration:500,easing: "swing", complete: function() {
+        $('#work').animate({height:'toggle'},500,"swing");
+      }
+    });
+    } else {
+      $('#work').animate({height:'toggle'},500,"swing");
+      $(this).toggleClass("active2")
+    };
+    $('#work-content').delay(700).fadeToggle(100);
+    $('.project-area').delay(1000).fadeToggle(100);
+    $('.links2 > a:nth-child(1)').removeClass("active1");
+    $(this).toggleClass("active2");
   });
 
 });
