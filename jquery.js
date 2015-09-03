0b0c95dcb30bdb9e5d6ca4f6f5bb85edabbd7f1a
mlaugh4 <mwlaugh@gmail.com>
2015-09-03 10:01:00 -0700

    yaaa

--- jquery.js
+++ jquery.js
@@ -1,24 +1,37 @@
 $(document).ready(function(){
 
   $('.links2 > a:nth-child(1)').click(function(){
-    // $('.container div:nth-child(2)').delay(500).hide(500);
-    $('#mark').animate({width:'toggle',opacity:'toggle'},500,"swing").delay(200);
-    $(this).toggleClass("active1")
+    if ( $('#work').is(':visible')) {
+      $('#work-content').fadeToggle(100);
+      $('.project-area').fadeToggle(100);
+      $('#work').animate({height:'toggle',opacity:'toggle'},{duration:500,easing: "swing", complete: function() {
+        $('#mark').animate({width:'toggle',opacity:'toggle'},500,"swing");
+        }
+      });
+    } else {
+      $('#mark').animate({width:'toggle',opacity:'toggle'},500,"swing");
+    };
+    $('#mark-content').delay(700).fadeToggle(100);
+    $('.links2 > a:nth-child(2)').removeClass("active2");
+    $(this).toggleClass("active1");
   });
+
 
   $('.links2 > a:nth-child(2)').click(function(){
     if ( $('#mark').is(":visible") ) {
       $('#mark').animate({width:'toggle',opacity:'toggle'},{duration:500,easing: "swing", complete: function() {
-        $('#work').animate({height:'toggle'},500,"swing");
+        $('#work').animate({height:'toggle',opacity:'toggle'},500,"swing");
       }
     });
     } else {
-      $('#work').animate({height:'toggle'},500,"swing");
+      $('#work').animate({height:'toggle',opacity:'toggle'},500,"swing");
     };
     $('#work-content').delay(700).fadeToggle(100);
     $('.project-area').delay(1000).fadeToggle(100);
     $('.links2 > a:nth-child(1)').removeClass("active1");
     $(this).toggleClass("active2");
   });
+
+
 
 });
